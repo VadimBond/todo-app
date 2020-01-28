84797aad26b0d097e8554ca1dd9d73c77315c22f
Vadim Bond <wadimbond@mail.ru>
2020-01-29 00:03:19 +0100

    part 3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
 
-console.log('Hello');
+import App from "./components/app";
+
+ReactDom.render(<App />, document.getElementById("root"));
