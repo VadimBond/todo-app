3cc4529d38b2144c173d60128cd4e64f33984829
Vadim Bond <wadimbond@mail.ru>
2020-08-07 00:06:43 +0200

    moved botstrap file

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDom from "react-dom";
 
 import App from "./components/app";
 
+// with Sketchy theme from Bootstrap
+import "./bootstrap.min.css";
+
 ReactDom.render(<App />, document.getElementById("root"));
