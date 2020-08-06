d85f14f4b1d6b64443076af27c7d16c044c05045
Vadim Bond <wadimbond@mail.ru>
2020-08-06 20:48:05 +0200

    Updates

---
+++ precache-manifest.2094992d9917944de9d87b398448bb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f6b7249c78285a224f0c71c68b0337",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "701738ff5cd3b5ea7c58",
+    "url": "/todo-app/static/css/main.7305ec61.chunk.css"
+  },
+  {
+    "revision": "58d1bb5d463c7b7f1f67",
+    "url": "/todo-app/static/js/2.a1bb6cb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.a1bb6cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701738ff5cd3b5ea7c58",
+    "url": "/todo-app/static/js/main.4b54471f.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
